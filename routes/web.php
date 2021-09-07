447f9a5153dcbc3d2aa1303f8c69423880771eec
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-09-07 22:06:39 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('post', PostController::class);
